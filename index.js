0a69170fb837f863740d71cc0b0b001e6133059a
Abdulaziz_c5 <abdulazizbekabdusodiqov@gmail.com>
2024-11-12 15:33:27 +0500

    LAST2 finaly push

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-import express from "express";
-import { config } from "dotenv";
-import { abdulazizdev } from "./src/router/index.routes.js";
+import express from 'express';
+import { config } from 'dotenv';
+import path from "path"
+import { abdulazizdev } from './src/router/index.routes.js';
 
 config(); // .env faylini yuklash
 
 const app = express();
 
-app.use(express.json());
-app.use(express.static('src/public'));
+// Statik fayllarni xizmatga qo'yish
+app.use(express.static(path.join(process.cwd(), "src", "public"))); // 'public' papkasidan xizmat ko'rsatish
 
+// Route-lar
+app.use('/', abdulazizdev);
 
-app.use("/", abdulazizdev);
+// 404 Error handling
+app.use((req, res) => {
+  return res.status(404).send('Not found Abdulaziz dev');
+});
 
-
-app.use((req, res)=>{
-  return res.status(404).send("not found Abdulaziz dev")
-})
-app.use((err, req, res, next)=>{
-  if(err){
-    res.send("error")
+// Global error handling
+app.use((err, req, res, next) => {
+  if (err) {
+    res.status(500).send('Internal Server Error');
   }
-})
+});
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, (err)=>{
-  if(err){
+app.listen(PORT, (err) => {
+  if (err) {
     console.log(err);
   }
-  console.log("Server listing on port:" + PORT);
-})
+  console.log('Server listening on port ' + PORT);
+});
