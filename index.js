28bb53d0c37f6f44f7f59b89b028020f7c5793ca
AbdulazizbekAbdusodiqov <abdulazizbekabdusodiqov@gmail.com>
2025-02-26 09:04:07 +0500

    update

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 import express from 'express';
-import { config } from 'dotenv';
 import path from "path"
 import { abdulazizdev } from './src/router/index.routes.js';
 
-config(); // .env faylini yuklash
-
 const app = express();
 
-// Statik fayllarni xizmatga qo'yish
-app.use(express.static(path.join(process.cwd(), "src", "public"))); // 'public' papkasidan xizmat ko'rsatish
+app.use(express.static(path.join(process.cwd(), "src", "public"))); 
 
-// Route-lar
-app.use('/abdulaziz-dev', abdulazizdev);
+app.use('/', abdulazizdev);
 
-// 404 Error handling
 app.use((req, res) => {
   return res.status(404).send('Not found Abdulaziz dev');
 });
 
-// Global error handling
 app.use((err, req, res, next) => {
   if (err) {
     res.status(500).send('Internal Server Error');
   }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = 80
 
 app.listen(PORT, (err) => {
   if (err) {
--- src/router/abdulazizdev.routes.js
+++ src/router/abdulazizdev.routes.js
@@ -3,6 +3,6 @@ import {aboutCon, homeCon, contactCon} from '../controller/index.controller.js'
 
 export const abdulazizdev =  express.Router()
 
-abdulazizdev.get('/home', homeCon)
+abdulazizdev.get('/', homeCon)
 abdulazizdev.get('/about', aboutCon)
 abdulazizdev.get('/contact', contactCon)
